21493aa01372babf5b4944d1ba159c856917dc13
Patrickeon <01051188129e@gmail.com>
2021-12-01 22:28:31 +1000

    all done react redux

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const toDos = createSlice({
+  name: "ToDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) => state.filter(toDo => toDo !== action.payload),
+  },
+});
+
+export const { add, remove } = toDos.actions;
+
+export default configureStore({ reducer: toDos.reducer });
